e9de0bce37d4cc46e865c3f7fe2df97675925f8e
NxFi3 <alirezajk1400@gmail.com>
2026-09-04 17:35:45 +0330

    Memory update

---
+++ src/Memory/MemoryCache.py
@@ -0,0 +1,79 @@
+#src/Memory/MemoryCache.py
+
+from src.Utils.logger import get_logger
+from src.Memory.MemoryEvent import MemoryEvent
+import pickle 
+import os 
+logger = get_logger('[CACHE]')
+
+
+class MemoryCache:
+    def __init__(self) -> None:
+        self.cache_path = 'MemoryTemporalCache.pkl' # Hard Path
+        self.database = self._readpath()
+        self.counts = len(self.database)
+    def _readpath(self):
+        try:
+            if os.path.exists(self.cache_path):
+                with open(self.cache_path,'rb') as f:
+                    return pickle.load(f)
+            else:
+                return []
+        except Exception as e:
+            logger.error(f'Error While Reading Cache : {e} Cannot Load Pervious Cache File')
+            return [] 
+        
+    def SaveTemporalCache(self):
+        try:
+            directory = os.path.dirname(self.cache_path)
+
+            if directory:
+                os.makedirs(directory, exist_ok=True)
+
+            with open(self.cache_path, 'wb') as f:
+                pickle.dump(self.database, f)
+
+            return True
+
+        except Exception as e:
+            logger.error(f'Cant Save TemporalCache Error: {e}')
+            return False
+        
+    def flush(self):
+        self.database = []
+        self.counts = 0
+
+        try:
+            if os.path.exists(self.cache_path):
+                os.remove(self.cache_path)
+            return True
+        except Exception as e:
+            logger.error(f'Cannot Flush Cache From Disk: {e}')
+            return False
+    
+    def is_empty(self):
+        return len(self.database) == 0
+    def should_consolidate(self):
+        if len(self.database) >=100:
+            logger.warning(f'Cache OverFlow Require consolidation')
+            return True
+        else:
+            return False        
+    def getlen(self):
+
+        return len(self.database)
+    
+    def add(self,Memory:MemoryEvent):
+        if not isinstance(Memory,MemoryEvent):
+            logger.warning(f'invalid Format Type {type(Memory)}')
+            return False 
+        
+        self.database.append(Memory)
+        self.counts = len(self.database)
+        if self.counts % 10 ==0:
+            self.SaveTemporalCache()
+            logger.info('Saving Cache')
+        return True
+    
+    def getall(self):
+        return self.database
--- src/Memory/MemoryItem.py
+++ src/Memory/MemoryItem.py
@@ -1,26 +1,18 @@
-#src/Memory/MemoryItems.py
 from dataclasses import dataclass
-import numpy as np
 from typing import Optional
+
+import numpy as np
+
 
 @dataclass
 class MemoryItem:
-    """ item for Memory """
     id: int
-    graph: np.ndarray
+    graph: Optional[np.ndarray]
     mem_type: str
     value: str
     embedding: np.ndarray
-    created_at: int        
-    last_access: int     
+    created_at: int
+    last_access: int
     count: int
     importance: float
-    deleted: int           
-    raw_score: Optional[float] =None
-    normalized_score: Optional[float] =None
-    similarity:Optional[float]=None
-    rank:Optional[float]=None
-    rrf_score:Optional[float] =None
-    bm25_rank: Optional[int] = None 
-    embedding_rank: Optional[int] = None 
-    embedding_distance:Optional[float] = None
+    deleted: int
