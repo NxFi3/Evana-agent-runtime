b65640affcd0e41d1acf35c33e61b0e0b1583c89
NxFi3 <alirezajk1400@gmail.com>
2026-09-06 17:17:02 +0330

    Tokenbudget update

--- src/Context/TokenBudget.py
+++ src/Context/TokenBudget.py
@@ -9,35 +9,27 @@ class TokenBudget:
     def __init__(self, config:Dict[str, Any],llm_provider:LlmProvider) -> None:
         self.config = config 
         self.llm_provider = llm_provider
-        self.budget = self.config.get("token_budget", 30000)
-        self.safe_marigin = self.config.get("safe_margin", 1000)
+        self.budget = self.config.get("token_budget", -1)
+        self.safe_marigin = self.config.get("safe_margin", -1)
+        if self.safe_marigin < 0:
+            logger.warning("Safe margin is set to -1,using 10% of model's context length as safe margin.")
+            self.safe_marigin =  (self.llm_provider.model.get_model_context_length()) * 0.1
         self.token_budget_previous = 0
-    def get_model_context_length(self):
-        try:
-            logger.info("Fetching model context length")
-
-            model_info = self.llm_provider.show_model_info()
-
-            if model_info is None:
-                return None
-
-            for key, value in model_info.modelinfo.items():
-                if key.endswith(".context_length"):
-                    return value
-
-            logger.warning(
-                f"Context length not found for model: {self.model_name}"
-            )
-            return None
-
-        except Exception as e:
-            logger.error(f"Error fetching model context length: {e}")
-            return None
-
     def remaining_budget(self,ModelResponse:Dict[str,Any]) -> int:
-        used_tokens = ModelResponse.get('prompt_eval_count',0)
-
-        return self.budget - used_tokens
+        try:
+            if self.budget < 0:
+                logger.warning("Token budget is set to -1, Usinng model's context length as budget.")
+                max_tokens = self.llm_provider.model.get_model_context_length()
+                used_tokens = ModelResponse.get('prompt_eval_count',0)
+                return max_tokens - used_tokens
+            else:
+                used_tokens = ModelResponse.get('prompt_eval_count',0)
+                return self.budget - used_tokens
+            
+        except Exception as e:
+            logger.error(f"Error calculating remaining budget: {e}")
+            return 0
+        
     def is_within_budget(self,ModelResponse:Dict[str,Any]) -> bool:
         remaining = self.remaining_budget(ModelResponse)
         if remaining < self.safe_marigin:
