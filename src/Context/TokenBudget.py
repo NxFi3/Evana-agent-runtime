57b550dded3be86ec81507e01142175e304f8711
NxFi3 <alirezajk1400@gmail.com>
2026-09-06 11:22:48 +0330

    incomplete

--- src/Context/TokenBudget.py
+++ src/Context/TokenBudget.py
@@ -4,24 +4,43 @@ from typing import Any, Dict
 from src.Utils.logger import get_logger 
 from src.Engine.llmManagment.LlmProvider import LlmProvider 
 logger = get_logger("[TOKENBUDGET]")
-class TokenBudge:
-    def __init__(self,max_size:int,llmprovider:LlmProvider) -> None:
-        self.ContextValue = 0
-        self.max_size = max_size
-        self.llmprovider = llmprovider
-        self.provider = llmprovider.provider_type
-    def compute_token_budget(self,ModelResponse:Dict[Any,object]) -> int:
-        if self.provider == "ollama":
-            self.ContextValue = ModelResponse.get('prompt_eval_count',0)
-        return self.ContextValue
-    def Compute_Remaining_budget(self,ModelResponse:Dict[Any,object]) -> int:
-        if self.provider == "ollama":
-            self.ContextValue = ModelResponse.get('prompt_eval_count',0)
-            remaining_budget = self.max_size - self.ContextValue
-        return remaining_budget
 
-    def is_budget_exceeded(self,ModelResponse:Dict[Any,object]) -> bool:
-        self.compute_token_budget(ModelResponse)
-        return self.ContextValue > self.max_size
+class TokenBudget:
+    def __init__(self, config:Dict[str, Any],llm_provider:LlmProvider) -> None:
+        self.config = config 
+        self.llm_provider = llm_provider
+        self.budget = self.config.get("token_budget", 30000)
+        self.safe_marigin = self.config.get("safe_margin", 1000)
+        self.token_budget_previous = 0
+    def get_model_context_length(self):
+        try:
+            logger.info("Fetching model context length")
 
-    
+            model_info = self.llm_provider.show_model_info()
+
+            if model_info is None:
+                return None
+
+            for key, value in model_info.modelinfo.items():
+                if key.endswith(".context_length"):
+                    return value
+
+            logger.warning(
+                f"Context length not found for model: {self.model_name}"
+            )
+            return None
+
+        except Exception as e:
+            logger.error(f"Error fetching model context length: {e}")
+            return None
+
+    def remaining_budget(self,ModelResponse:Dict[str,Any]) -> int:
+        used_tokens = ModelResponse.get('prompt_eval_count',0)
+
+        return self.budget - used_tokens
+    def is_within_budget(self,ModelResponse:Dict[str,Any]) -> bool:
+        remaining = self.remaining_budget(ModelResponse)
+        if remaining < self.safe_marigin:
+            logger.warning(f"Token budget exceeded. Remaining tokens: {remaining}. Safe margin: {self.safe_marigin}")
+            return False
+        return True
