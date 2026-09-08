9c4a7ac4e79a2d7bb5e69ce5097d3d351b555ef8
NxFi3 <alirezajk1400@gmail.com>
2026-09-08 12:37:33 +0330

    .

--- src/Memory/MemoryEvent.py
+++ src/Memory/MemoryEvent.py
@@ -1,13 +1,18 @@
 #src/Memory/MemoryEvent.py
-from dataclasses import dataclass
-from typing import Dict 
+
+from dataclasses import dataclass, field
+from typing import Dict
 from datetime import datetime
+from uuid import UUID, uuid4
+
+
 @dataclass
 class MemoryEvent:
-    id:int
-    event_type:str
-    content:str
-    source:str
-    step:int
-    timestamp:datetime
-    metadata:Dict[str, object]
+    id: UUID = field(default_factory=uuid4)
+
+    event_type: str = ""
+    content: str = ""
+    source: str = ""
+    step: int = 0
+    timestamp: datetime = field(default_factory=datetime.now)
+    metadata: Dict[str, object] = field(default_factory=dict)
